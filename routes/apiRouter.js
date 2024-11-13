57cb67835c76b51e18eb64f25d7d9d6df2015567
satyam prajapati <satyamprajapati1979@gmail.com>
2024-11-13 11:14:52 +0530

    updating package.json , from index.js to server.json on  main

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -2,6 +2,9 @@ import { Router } from "express";
 import panValidation from "../controllers/apiControl.js";
  
 const router = Router()
+router.get('/', (req, res)=> {
+   res.send("<h2>you are on /api router </h2>")
+})
 
 router.get('/:panNumber', panValidation)
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import path from 'path'
 const app = express();
 const port = process.env.PORT || 3000;
   
